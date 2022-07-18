712cdacaeed493ff80ef083bc69451826a5653b6
Kyle Tillson <kyletillson004@gmail.com>
2022-07-18 15:56:55 -0600

    commit tree 0fe0699cccc9ceb37ba6cdc80d55c369e40aad09

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if(someNumber <= 400){
+    return 'This one is on me!'
+  } else if(someNumber >= 400 && someNumber <= 2000){
+    return 'That will be twenty bucks.'
+  } else if(someNumber > 2000 && someNumber <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if(someNumber > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' :'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch(amount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+        
+  }
+
 }
